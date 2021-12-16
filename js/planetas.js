7a7ec9c6ca09eb743b0491d68d88ccce9ef11722
Jose Mario <josemariog99@gmail.com>
2021-12-16 11:26:38 -0500

    Agregando metodos de ordenar y buscar

--- js/planetas.js
+++ js/planetas.js
@@ -1,6 +1,15 @@
 document.querySelector('#btn_enviar_evaluacion').addEventListener('click', function(){
     guardarDatos();
 });
+
+document.querySelector('#btn_ordenar_valores').addEventListener('click', function(){
+    ordenarPlanetas();
+});
+
+document.querySelector('#btn_buscar_planeta').addEventListener('click', function(){
+    buscarPlaneta();
+});
+
  
 function guardarDatos(){
     
@@ -32,28 +41,28 @@ function guardarDatos(){
         let planetas = [
             {
                 "nombre": `${nomPlaneta1}`,
-                "distancia": `${disPlaneta1}`,
-                "tamaño": `${tamPlaneta1}`
+                "distancia": parseFloat(`${disPlaneta1}`),
+                "tamaño": parseFloat(`${tamPlaneta1}`)
             },
             {
                 "nombre": `${nomPlaneta2}`,
-                "distancia": `${disPlaneta2}`,
-                "tamaño": `${tamPlaneta2}`
+                "distancia": parseFloat(`${disPlaneta2}`),
+                "tamaño": parseFloat(`${tamPlaneta2}`)
             },
             {
                 "nombre": `${nomPlaneta3}`,
-                "distancia": `${disPlaneta3}`,
-                "tamaño": `${tamPlaneta3}`
+                "distancia": parseFloat(`${disPlaneta3}`),
+                "tamaño": parseFloat(`${tamPlaneta3}`)
             },
             {
                 "nombre": `${nomPlaneta4}`,
-                "distancia": `${disPlaneta4}`,
-                "tamaño": `${tamPlaneta4}`
+                "distancia": parseFloat(`${disPlaneta4}`),
+                "tamaño": parseFloat(`${tamPlaneta4}`)
             },
             {
                 "nombre": `${nomPlaneta5}`,
-                "distancia": `${disPlaneta5}`,
-                "tamaño": `${tamPlaneta5}`
+                "distancia": parseFloat(`${disPlaneta5}`),
+                "tamaño": parseFloat(`${tamPlaneta5}`)
             }
         ];
 
@@ -61,6 +70,43 @@ function guardarDatos(){
         localStorage.setItem('ArrayPlanetas', JSON.stringify(planetas));
         alert("Información guardada")
 }
+
+function ordenarPlanetas(){
+    
+    if(localStorage.getItem('ArrayPlanetas')){
+
+        let listaPlanetas = JSON.parse(localStorage.getItem('ArrayPlanetas'));
+
+        listaPlanetas.sort((a, b) => {
+            if (a.distancia < b.distancia){
+                return -1;
+            } else if (a.distancia > b.distancia){
+                return 1;
+            }
+        });
+    
+        console.log(listaPlanetas);
+
+
+    } else {
+        console.log('No se encontraron los datos');
+    }
+}
+
+function buscarPlaneta(){
+    
+    let listaPlanetas = JSON.parse(localStorage.getItem('ArrayPlanetas'));
+
+    let busqueda = prompt("¿Qué planeta desea buscar?");
+    
+    const resultado = listaPlanetas.find(elemento => {
+        return elemento.nombre == busqueda;
+
+    });
+    console.log(resultado);
+    
+}
+
 
 
 
