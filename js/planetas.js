c1cb80437f26299c887072d812579831edd8e525
Jose Mario <josemariog99@gmail.com>
2021-12-15 23:46:32 -0500

    Agregando estilos css

---
+++ js/planetas.js
@@ -0,0 +1,33 @@
+document.querySelector('#btn_enviar_evaluacion').addEventListener('click', function(){
+
+    // Planeta 1
+    let nomPlaneta1 = document.getElementById('np_planeta_1').value;
+    let disPlaneta1 = document.getElementById('d_planeta_1').value;
+    let tamPlaneta1 = document.getElementById('t_planeta_1').value;
+
+    // Planeta 2
+    let nomPlaneta2 = document.getElementById('np_planeta_2').value;
+    let disPlaneta2 = document.getElementById('d_planeta_2').value;
+    let tamPlaneta2 = document.getElementById('t_planeta_2').value;
+
+    // Planeta 3
+    let nomPlaneta3 = document.getElementById('np_planeta_3').value;
+    let disPlaneta3 = document.getElementById('d_planeta_3').value;
+    let tamPlaneta3 = document.getElementById('t_planeta_3').value;
+
+    // Planeta 4
+    let nomPlaneta4 = document.getElementById('np_planeta_4').value;
+    let disPlaneta4 = document.getElementById('d_planeta_4').value;
+    let tamPlaneta4 = document.getElementById('t_planeta_4').value;
+
+    // Planeta 5
+    let nomPlaneta5 = document.getElementById('np_planeta_5').value;
+    let disPlaneta5 = document.getElementById('d_planeta_5').value;
+    let tamPlaneta5 = document.getElementById('t_planeta_5').value;
+
+    alert("Se guardaron los datos");
+
+});
+
+
+
